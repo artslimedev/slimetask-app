ac7b2817dc7492685f8c35f99adfc4b97f4a5dab
artslimedev <102567983+artslimedev@users.noreply.github.com>
2025-04-22 16:55:42 -0400

    Adding go version

---
+++ .eslintrc.js
@@ -0,0 +1,25 @@
+module.exports = {
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/recommended",
+    "next/core-web-vitals",
+  ],
+  rules: {
+    // Global rules
+    "@typescript-eslint/no-unused-expressions": "warn",
+    "@typescript-eslint/no-require-imports": "warn",
+    "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/no-empty-object-type": "warn",
+  },
+  overrides: [
+    {
+      files: ["lib/**/*.{ts,tsx,js,d.ts}"],
+      rules: {
+        "@typescript-eslint/no-unused-expressions": "off",
+        "@typescript-eslint/no-require-imports": "off", // ✅ disabled here
+        "@typescript-eslint/no-explicit-any": "off",
+        "@typescript-eslint/no-empty-object-type": "off",
+      },
+    },
+  ],
+};
